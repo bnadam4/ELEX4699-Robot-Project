b5fc21575dbbadb0369903895435dc6a5d859e6e
Bryce Adam <bnadam4@gmail.com>
2024-04-22 13:08:30 -0700

    Added Car.h top level file

---
+++ Car.h
@@ -0,0 +1,16 @@
+#include "Server.h"
+#include "Motor.h"
+#include "Guidance.h"
+class CCar
+{
+private:
+ CServer _server;
+ CMotor _motors;
+ CGuidance _guidance;
+ static void serverthrd(CCar *ptr);
+ static void imagethrd(CCar *ptr);
+public:
+ CCar();
+ ~CCar();
+ void drive();
+};
